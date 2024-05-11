5c9e668ba3845350a3b99165a9c6707d9f1cbca0
fastgameplay <61421145+fastgameplay@users.noreply.github.com>
2024-05-11 05:24:28 +0400

    0.0.1 Scriptable Events

---
+++ Assets/Game/Scripts/Global/Events/BasicSharpEvents/SO_BoolEvent.cs
@@ -0,0 +1,6 @@
+namespace ScriptableEvents.Base{
+    using UnityEngine;
+    
+    [CreateAssetMenu(fileName = "Void Event", menuName = "Events/CSharp/Void")]
+    public class SO_BoolEvent : SO_BaseEvent<bool> {}
+}
